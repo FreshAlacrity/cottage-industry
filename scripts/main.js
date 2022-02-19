83b1e3b2207aa13874fb55027d4741b8ac3e879b
Alec <freshalacrity@gmail.com>
2022-02-19 13:48:59 -0500

    Moving scripts to https://freshalacrity.github.io/cottage-industry

--- scripts/main.js
+++
@@ -1,4 +0,0 @@
-fetch('minecraftinstance.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
-document.getElementById("featured-list").innerHTML = "Featured List Here";
